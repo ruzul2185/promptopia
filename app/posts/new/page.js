a7404a8246fc65bcedf22b0c38ba314430fd976b
ruzul2185 <136430323+ruzul2185@users.noreply.github.com>
2024-10-01 18:58:08 +0530

    Home created

--- app/posts/new/page.js
+++
@@ -1,9 +0,0 @@
-import react from 'react'
-
-const page = () => {
-    return(
-        <h1>NEW POSTS!</h1>
-    )
-}
-
-export default page;
--- app/posts/page.js
+++
@@ -1,9 +0,0 @@
-import react from 'react'
-
-const page = () => {
-    return(
-        <h1>POSTS!</h1>
-    )
-}
-
-export default page;
