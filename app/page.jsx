584f4a79e5fd7cdb3b58f7678eb502d848f5030e
ruzul2185 <136430323+ruzul2185@users.noreply.github.com>
2024-10-02 01:35:59 +0530

    Home Page created and nav bar made

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Feed from '@/components/Feed'
 
 const Home = () => {
     return (
@@ -9,7 +10,7 @@ const Home = () => {
                 Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
             </p>
 
-            {/*feed*/}
+            <Feed />
         </section>
     )
 }
---
+++ components/Feed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feed = () => {
+    return(
+        <h2>
+            feed
+        </h2>
+    )
+}
+
+export default Feed
