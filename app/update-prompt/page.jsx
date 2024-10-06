096a1fe060c1e113bb639487427f7c758b11e530
GardiansOfGalaxy <galaxy14072000@gmail.com>
2024-10-06 20:58:31 +0530

    build issue updated-promts fix

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,75 +1,12 @@
-'use client'
+// app/update-prompt/page.js
 
-import {useEffect, useState} from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import React, { Suspense } from 'react';
+import EditPrompt from './EditPrompt';
 
-import Form from '@/components/Form';
-
-
-const EditPrompt = () => {
-    const [submitting, setSubmitting] = useState(false)
-    const [post, setPost] = useState({
-        prompt:'',
-        tag:''
-    })
-    const searchParams = useSearchParams()
-    const promptId = searchParams.get('id')
-
-    useEffect(() => {
-        const getPromptDetails = async() => {
-            const response = await fetch(`/api/prompt/${promptId}`)
-            const data = await response.json()
-            setPost({
-                prompt: data.prompt,
-                tag: data.tag,
-            })
-        }
-
-        if(promptId) {
-            getPromptDetails()
-        }
-
-    },[promptId])
-
-    const router = useRouter()
-
-    const updatePrompt = async(e) => {
-        e.preventDefault()
-        setSubmitting(true)
-
-        if(!promptId) {
-            return alert('Prompt ID not found')
-        }
-
-        try{
-            const response = await fetch(`/api/prompt/${promptId}`,{
-                method:'PATCH',
-                body: JSON.stringify({
-                    prompt: post.prompt,
-                    tag: post.tag,
-                })
-            })
-            if (response.ok) {
-                router.push('/')
-            }
-        }catch(error) {
-            console.log(error)
-        }finally {
-            setSubmitting(false)
-        }
-    }
-
+export default function UpdatePromptPage() {
     return (
-        <div>
-            <Form
-                type='Edit'
-                post={post}
-                setPost={setPost}
-                submitting={submitting}
-                handleSubmit={updatePrompt}
-            />
-        </div>
-    )
+        <Suspense fallback={<div>Loading...</div>}>
+            <EditPrompt />
+        </Suspense>
+    );
 }
-
-export default EditPrompt
